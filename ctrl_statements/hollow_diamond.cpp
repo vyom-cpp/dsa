fad5b057671285e251a1f6acfae18610d9020df9
vyom-cpp <vyom.sutariya0948@gmail.com>
2026-05-07 16:36:09 +0530

    Loops completed

--- ctrl_statements/hollow_diamond.cpp
+++ ctrl_statements/hollow_diamond.cpp
@@ -25,5 +25,23 @@ int main()
         }
         cout << endl;
     }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+        if (i != n - 2)
+        {
+            for (int j = 0; j < 2 * (n - i) - 5; j++)
+            {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
     return 0;
 }
