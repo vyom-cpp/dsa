6dbe613be6010bad26648f5b7873f972f74f536c
vyom-cpp <vyom.sutariya0948@gmail.com>
2026-06-02 18:45:44 +0530

    Solved Pow(x, n) problem within the given constraints

---
+++ leetcode/arrays/pow.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+double myPow(double x, int n)
+{
+    if (n == 0)
+        return 1.0;
+    if (x == 0)
+        return 0.0;
+    if (x == 1)
+        return 1.0;
+    if (x == -1 && n % 2 == 0)
+        return 1.0;
+    if (x == -1 && n % 2 != 0)
+        return -1.0;
+    
+    long binForm = n;
+    if (n < 0)
+    {
+        x = 1 / x;
+        binForm = -binForm;
+    }
+    double ans = 1;
+    while (binForm > 0)
+    {
+        if (binForm % 2 == 1)
+        {
+            ans *= x;
+        }
+        x *= x;
+        binForm /= 2;
+    }
+    return ans;
+}
+
+int main()
+{
+    cout << myPow(3, 5) << endl;
+    return 0;
+}
