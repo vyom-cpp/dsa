bffca98a32fd7df46f6af00ea577c111cdbd74da
vyom-cpp <vyom.sutariya0948@gmail.com>
2026-06-08 17:32:59 +0530

    binary search in recursive part

--- pointers/binary-search.cpp
+++ pointers/binary-search.cpp
@@ -2,38 +2,59 @@
 #include <vector>
 using namespace std;
 
-int binary_search(vector<int> &nums, int target)        // Iterative Code
+// int binary_search(vector<int> &nums, int target)        // Iterative Code
+// {
+//     int left = 0;
+//     int right = nums.size() - 1;
+
+//     while (left <= right)
+//     {
+//         int mid = left + (right - left) / 2;
+
+//         if (nums[mid] == target)
+//         {
+//             return mid;
+//         }
+//         else if (nums[mid] < target)
+//         {
+//             left = mid + 1;
+//         }
+//         else
+//         {
+//             right = mid - 1;
+//         }
+//     }
+
+//     return -1;
+// }
+
+int binary_search(vector<int> &nums, int target, int start, int end) // Recursive Code
 {
-    int left = 0;
-    int right = nums.size() - 1;
-
-    while (left <= right)
+    if (start <= end)
     {
-        int mid = left + (right - left) / 2;
-
-        if (nums[mid] == target)
+        int mid = start + (end - start) / 2;
+        if (target > nums[mid])
         {
-            return mid;
+            return binary_search(nums, target, mid + 1, end);
         }
-        else if (nums[mid] < target)
+        else if (target < nums[mid])
         {
-            left = mid + 1;
+            return binary_search(nums, target, start, mid - 1);
         }
         else
         {
-            right = mid - 1;
+            return mid;
         }
     }
-
     return -1;
 }
 
 int main()
 {
-    vector<int> nums = {-1, 0, 4, 5, 9, 12};
-    int target;
+    vector<int> nums = {-1, 0, 4, 5, 9, 12, 13, 19, 21};
+    int target, start = 0, end = nums.size() - 1;
     cout << "Enter the target: ";
     cin >> target;
-    cout << binary_search(nums, target) << endl;
+    cout << binary_search(nums, target, start, end) << endl;
     return 0;
 }
