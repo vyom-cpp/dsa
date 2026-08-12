e331ba409a6fce85fdff3906f184d1fa8afda2d5
vyom-cpp <vyom.sutariya0948@gmail.com>
2026-08-12 15:50:33 +0530

    completed leetcode #75, sort colors using dutch national flag algorithm

--- leetcode/sorting/colors.cpp
+++ leetcode/sorting/colors.cpp
@@ -3,25 +3,42 @@
 
 using namespace std;
 
-void sortColors(vector<int> & nums){
-    int n = nums.size();
-    int count0 = 0, count1 = 0, count2 = 0;
-    for(int i = 0; i < n; i++){
-        if(nums[i] == 0) count0++;
-        else if(nums[i] == 1) count1++;
-        else if(nums[i] == 2) count2++;
-    }
+// void sortColors(vector<int> & nums){
+//     int n = nums.size();
+//     int count0 = 0, count1 = 0, count2 = 0;
+//     for(int i = 0; i < n; i++){
+//         if(nums[i] == 0) count0++;
+//         else if(nums[i] == 1) count1++;
+//         else if(nums[i] == 2) count2++;
+//     }
 
-    int index = 0;
-    for(int i = 0; i < count0; i++){
-        nums[index++] = 0;
+//     int index = 0;
+//     for(int i = 0; i < count0; i++){
+//         nums[index++] = 0;
+//     }
+//     for(int i = 0; i < count1; i++){
+//         nums[index++] = 1;
+//     }
+//     for(int i = 0; i < count2; i++){
+//         nums[index++] = 2;
+//     }    
+// }
+
+// this is the most optimized approach, dnf algorithm (dutch national flag)
+void sortColors(vector<int> &nums){
+    int low = 0, mid = 0, n = nums.size(), high = n - 1;
+    while(mid <= high){
+        if(nums[mid] == 0){
+        swap(nums[low], nums[mid]);
+        low++;
+        mid++;
+    } else if(nums[mid] == 1){
+        mid++;
+    } else {
+        swap(nums[high], nums[mid]);
+        high--;
     }
-    for(int i = 0; i < count1; i++){
-        nums[index++] = 1;
     }
-    for(int i = 0; i < count2; i++){
-        nums[index++] = 2;
-    }    
 }
 
 int main() {
