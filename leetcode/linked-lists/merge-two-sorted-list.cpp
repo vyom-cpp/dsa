7aaeeaa273ddee563bac65c9d69918667f614294
vyom-cpp <vyom.sutariya0948@gmail.com>
2026-09-13 16:20:06 +0530

    sort the list using modified merge sort approach and merge the two sorted linked lists

--- leetcode/linked-lists/merge-two-sorted-list.cpp
+++ leetcode/linked-lists/merge-two-sorted-list.cpp
@@ -1,38 +1,109 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
+#include <iostream>
+using namespace std;
+
+class Node {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode(-1);
-        ListNode* res = dummy;
-        ListNode* t1 = list1;
-        ListNode* t2 = list2;
-        while(t1 != nullptr && t2 != nullptr){
-            if(t1->val <= t2->val){
-                res->next = t1;
-                t1 = t1->next;
-            } else {
-                res->next = t2;
-                t2 = t2->next;
-            }
-            res = res->next;
-        }
-        if(t1 != nullptr) {
-            res->next = t1;
-        } else {
-            res->next = t2;
-        }
-    
-        ListNode* actualHead = dummy->next;
-        delete dummy; 
-        return actualHead;
+    int data;
+    Node* next;
+
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
     }
 };
+
+class Solution {
+public:
+    Node* mergeTwoSortedLinkedLists(Node* list1, Node* list2) {
+        Node* dummyNode = new Node(-1);
+        Node* temp = dummyNode;
+
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->data <= list2->data) {
+                temp->next = list1;
+                list1 = list1->next;
+            } else {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp->next;
+        }
+
+        if (list1 != nullptr) {
+            temp->next = list1;
+        } else {
+            temp->next = list2;
+        }
+
+        return dummyNode->next;
+    }
+
+    Node* findMiddle(Node* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        Node* slow = head;
+        Node* fast = head->next;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+
+    Node* sortLL(Node* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        Node* middle = findMiddle(head);
+
+        Node* right = middle->next;
+        middle->next = nullptr;
+        Node* left = head;
+
+        left = sortLL(left);
+        right = sortLL(right);
+
+        return mergeTwoSortedLinkedLists(left, right);
+    }
+};
+
+void printLinkedList(Node* head) {
+    Node* temp = head;
+
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = new Node(3);
+    head->next = new Node(2);
+    head->next->next = new Node(5);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(1);
+
+    cout << "Original Linked List: ";
+    printLinkedList(head);
+
+    Solution obj;
+
+    head = obj.sortLL(head);
+
+    cout << "Sorted Linked List: ";
+    printLinkedList(head);
+
+    return 0;
+}
