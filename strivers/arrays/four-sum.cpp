8dbd7c7db66f1125aaa28a0ec57c4096674706c8
vyom-cpp <vyom.sutariya0948@gmail.com>
2026-09-15 18:14:06 +0530

    four sum problem

---
+++ strivers/arrays/four-sum.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+vector<vector<int>> fourSumBrute(vector<int> arr, int target){
+    set<vector<int>> st;
+    for(int i = 0; i < arr.size(); i++){
+        for(int j = i + 1; j < arr.size(); j++){
+            for(int k = j + 1; k < arr.size(); k++){
+                for(int l = k + 1; l < arr.size(); l++){
+                    long long sum = arr[i] + arr[j] + arr[k] + arr[l];
+                    if(sum == target){
+                        vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+                }
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+
+vector<vector<int>> fourSumBetter(vector<int> arr, int target){
+    set<vector<int>> st;
+    for(int i = 0; i < arr.size(); i++){
+        for(int j = i + 1; j < arr.size(); j++){
+            set<long long> hashSet;
+            for(int k = j + 1; k < arr.size(); k++){
+                long long sum = arr[i] + arr[j] + arr[k];
+                long long fourth = target - sum;
+                if(hashSet.find(fourth) != hashSet.end()){
+                    vector<int> temp = {arr[i], arr[j], arr[k], (int)fourth};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+                hashSet.insert(arr[k]);     // inserting every element between j and k, checking one unique among them to be the fourth value
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());  // It creates a new 2D vector named ans and copies all elements from set st
+    return ans;
+}
+
+vector<vector<int>> fourSumOptimal(vector<int> arr, int target){
+    int n = arr.size();
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
+    for(int i = 0; i < n; i++){
+        // skip duplicates of i
+        if(i > 0 && arr[i] == arr[i-1])  continue;
+        for(int j = i + 1; j < n; j++){
+            // skip duplicates of j
+            if(j != (i + 1) && arr[j] == arr[j-1])  continue;
+            int k = j + 1, l = n - 1;
+            while(k < l){
+                long long sum = arr[i] + arr[j];
+                sum += arr[k];
+                sum += arr[l];  // this is done to avoid the overflow of case [1000000000,1000000000,1000000000,1000000000]
+                if(sum == target){
+                    vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
+                    ans.push_back(temp);
+                    k++;
+                    l--;
+                    while(k < l && arr[k] == arr[k-1])  k++;
+                    while(k < l && arr[l] == arr[l+1])  l--;
+                } else if(sum < target)     k++;
+                else    l--;
+            }
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> arr = {1, 0, -1, 0, -2, 2};
+    int target;
+    cin >> target;
+    vector<vector<int>> ans = fourSumOptimal(arr, target);
+    for (const auto& quadruplet : ans) {
+        cout << "[ ";
+        for (int num : quadruplet) {
+            cout << num << " ";
+        }
+        cout << "]\n";
+    }
+    return 0;
+}
